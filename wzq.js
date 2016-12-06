593aa69a69f199237638069bac4ddee352b00acf
1360002709@qq.com <1360002709@qq.com>
2016-12-06 14:36:29 +0800

    修改

--- wzq.js
+++ wzq.js
@@ -282,13 +282,13 @@ $(function(){
     //再来一局
     var again=$('.again');
 	again.on("click",function(){
-		again.css("background","aqua")
+		again.css("background","#ffe4c4")
 		window.location.reload();
 	});
 	//退出
 	 var cancel=$('.cancel');
 	cancel.on("click",function(){
-		cancel.css("background","aqua")
+		cancel.css("background","#ffe4c4")
 		window.close();
 	});
 	
