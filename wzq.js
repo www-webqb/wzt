39612533df4989924b0d783d593298c7fb9b156f
1360002709@qq.com <1360002709@qq.com>
2016-12-06 14:48:04 +0800

    xx

--- wzq.js
+++ wzq.js
@@ -282,13 +282,13 @@ $(function(){
     //再来一局
     var again=$('.again');
 	again.on("click",function(){
-		again.css("color","#ffe4c4")
+		again.css("color","red")
 		window.location.reload();
 	});
 	//退出
 	 var cancel=$('.cancel');
 	cancel.on("click",function(){
-		cancel.css("color","#ffe4c4")
+		cancel.css("color","red")
 		window.close();
 	});
 	
